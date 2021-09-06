8f2a54f236e25d6ca3bb05adcfa8109f58496d1e
mv-yurchenko <maxonus2@gmail.com>
2021-09-06 21:58:23 +0300

    wip

--- source/main.py
+++ source/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from loguru import logger
 from routes.v1 import v1_router
 
-# from controller.v1.utils import generate_secret_token
+from controller.v1.utils import generate_secret_token
 
 
 app = FastAPI()
@@ -26,8 +26,8 @@ app.include_router(v1_router)
 if __name__ == "__main__":
     if not os.getenv("SECRET_TOKEN"):
         logger.warning("Token not found.")
-        # token = generate_secret_token()
-        # os.environ["SECRET_TOKEN"] = token
-        # logger.info(f"Your token: '{token}'")
+        token = generate_secret_token() + generate_secret_token() + generate_secret_token()
+        os.environ["SECRET_TOKEN"] = token
+        logger.info(f"Your token: '{token}'")
 
     uvicorn.run(app, port=8000, host="0.0.0.0")
