8a7fd6f6a111f3eb6bf0ff4eeb83454d1d7384ac
mv-yurchenko <maxonus2@gmail.com>
2021-10-11 21:30:47 +0300

    Add tests for get installation

--- tests/installation/test_installation.py
+++ tests/installation/test_installation.py
@@ -33,18 +33,34 @@ async def test_create_installation(
 
 @pytest.mark.asyncio
 async def test_create_installation_name_already_exists(
-    client_with_submodules: AsyncClient,
+    client_with_test_installation: AsyncClient,
     create_installation_success_body: dict,
     create_installation_name_already_exists_response: dict,
 ):
-    (
-        await client_with_submodules.post(
-            "/v1/installation/", data=create_installation_success_body
-        )
-    ).json()
     response = (
-        await client_with_submodules.post(
+        await client_with_test_installation.post(
             "/v1/installation/", data=create_installation_success_body
         )
     ).json()
     assert response == create_installation_name_already_exists_response
+
+
+@pytest.mark.parametrize(
+    "with_credentials, expected_response",
+    [
+        (False, get_installation_no_credentials_response),  # noqa: F405
+        (True, get_installation_with_credentials_response),  # noqa: F405
+    ],
+)
+@pytest.mark.asyncio
+async def get_installation(
+    with_credentials: bool,
+    expected_response: dict,
+    client_with_test_installation: AsyncClient,
+):
+    response = (
+        await client_with_test_installation.get(
+            "/v1/installation/test-1", params={"with_credentials": with_credentials}
+        )
+    ).json()
+    assert response == expected_response
